997d12b13a545ce37268460aa5bbb38560d1ccf4
Sabin Thapa <sabint017@gmail.com>
2022-01-11 19:24:39 +0545

    add getStaticPath function #4

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,5 +1,23 @@
 // [id] is used because the page id is unique/dynamic
 
+// getStaticPaths runs at build time as well
+
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  const paths = data.map((project) => {
+    return {
+      params: { id: project.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 const ProjectDetails = () => {
   return (
     <div>
