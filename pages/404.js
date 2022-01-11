6977aaa58b01581508f1cb5657bd193b5d3ed990
Sabin Thapa <sabint017@gmail.com>
2022-01-11 16:47:31 +0545

    add automatic redirect to homepage from 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,21 @@
 import Link from "next/link";
 import React from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+  const [time, setTime] = useState(3);
+
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      setTime(time - 1);
+    }, 1000);
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, [time]);
+
   return (
     <div className="not-found">
       <h1> OOPS!!!</h1>
@@ -10,6 +24,7 @@ const NotFound = () => {
         {" "}
         Go back to <Link href="/"> Homepage</Link>
       </p>
+      <p> Automatic redirecting to the Homepage in {time} seconds...</p>
     </div>
   );
 };
