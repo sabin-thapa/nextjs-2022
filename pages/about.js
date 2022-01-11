09073b710f9df3d605dd7eea26ec0ada06d6f3fc
Sabin Thapa <sabint017@gmail.com>
2022-01-11 15:19:01 +0545

    add pages files

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <div>
+      <h1>This is the about page </h1>
+      <h3>
+        loremDeserunt mollit eiusmod in nostrud commodo occaecat eiusmod.
+        Officia do Lorem aliquip eiusmod voluptate. Cupidatat nisi deserunt
+        laboris proident ex. In laboris ullamco laborum ut amet aliquip tempor
+        dolor tempor enim commodo eiusmod cillum.
+      </h3>
+    </div>
+  );
+};
+
+export default about;
