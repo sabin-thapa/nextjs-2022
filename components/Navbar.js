42e1616d5129f0b1ef1a4f20d14ec47732869f01
Sabin Thapa <sabint017@gmail.com>
2022-01-11 16:59:20 +0545

    add Image:logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 import navStyles from "../styles/Navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={navStyles.navbar}>
       <div className={navStyles.logo}>
-        <h1> Sabin Thapa</h1>
+        <Image src="/logo.jpg" height={50} width={50} />
       </div>
       <ul className={navStyles.navItems}>
         <li>
