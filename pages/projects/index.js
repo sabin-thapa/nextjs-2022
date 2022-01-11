ebe4424e49e1a36f839381db55ba015aed16c2b0
Sabin Thapa <sabint017@gmail.com>
2022-01-11 18:47:02 +0545

    add styles to project list

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,13 +1,30 @@
-import Head from "next/head";
-import React from "react";
 import Title from "../../components/Title";
+import styles from "../../styles/Project.module.css";
 
-const Projects = () => {
+//This function runs before the component below renders || not like useEffect
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return {
+    props: {
+      posts: data,
+    },
+  };
+};
+
+const Projects = ({ posts }) => {
   return (
     <>
       <Title title="Projects" />
       <div>
         <h2> All my projects!</h2>
+        {posts.map((post) => (
+          <div key={post.id} className={styles.project}>
+            <h2> {post.title}</h2>
+            <p> {post.body} </p>
+          </div>
+        ))}
       </div>
     </>
   );
