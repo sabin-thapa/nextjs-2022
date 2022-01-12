5618a942945863b958b3a90585998aa567e1bfbb
Sabin Thapa <sabint017@gmail.com>
2022-01-12 17:57:36 +0545

    add getStaticPaths/Props

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,11 +1,35 @@
 import { API_URL } from "@/config/index";
 
-export const getStaticProps = async ({ query: { slug } }) => {
+// export const getServerSideProps = async ({ query: { slug } }) => {
+//   const res = await fetch(`${API_URL}/api/projects/${slug}`);
+//   const events = await res.json();
+
+//   return {
+//     props: { evt: events[0] },
+//   };
+// };
+
+//FOR STATIC GENERATION
+export const getStaticPaths = async () => {
+  const res = await fetch(`${API_URL}/api/projects/`);
+  const events = await res.json();
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
   const res = await fetch(`${API_URL}/api/projects/${slug}`);
   const events = await res.json();
 
   return {
     props: { evt: events[0] },
+    revalidate: 1,
   };
 };
 
